5017f2defadb9242b19a8b54fb44b6ad1c4ed708
Mxlan1 <milan.moerman@student.hogent.be>
2025-03-31 15:23:19 +0200

    EF gegenereerd.

---
+++ ReactApp1/Petanque.Storage/Spelverdeling.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Petanque.Storage;
+
+public partial class Spelverdeling
+{
+    public int SpelverdelingsId { get; set; }
+
+    public int? SpelId { get; set; }
+
+    public string Team { get; set; } = null!;
+
+    public string SpelerPositie { get; set; } = null!;
+
+    public int SpelerVolgnr { get; set; }
+}
