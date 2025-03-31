76e1933c7522a635fccbccaa5d9e133d89669fa8
Mxlan1 <milan.moerman@student.hogent.be>
2025-03-31 16:21:10 +0200

    Problemen proberen oplossen met de Git.

---
+++ ReactApp1/Petanque.Services/IPlayerService.cs
@@ -0,0 +1,15 @@
+﻿using Petanque.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petanque.Services
+{
+    public interface IPlayerService
+    {
+        PlayerResponseContract? GetById(int id);
+        PlayerResponseContract Create(PlayerRequestContract request);
+    }
+}
---
+++ ReactApp1/Petanque.Services/PlayerService.cs
@@ -0,0 +1,50 @@
+﻿using Petanque.Contracts;
+using Petanque.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petanque.Services
+{
+    public class PlayerService(Id312896PetanqueContext context) : IPlayerService
+    {
+        public PlayerResponseContract Create(PlayerRequestContract request)
+        {
+            var entity = new Speler()
+            {
+                Voornaam = request.Voornaam,
+                Naam = request.Naam,
+                Aanwezigheids = (ICollection<Aanwezigheid>)request.Aanwezigheids,
+                Dagklassements = (ICollection<Dagklassement>)request.Dagklassements,
+                Seizoensklassements = (ICollection<Seizoensklassement>)request.Seizoensklassements
+            };
+
+            context.Spelers.Add(entity);
+            context.SaveChanges();
+
+            return MapToContract(entity);
+        }
+
+        public PlayerResponseContract? GetById(int id)
+        {
+            var entity = context.Spelers.Find(id);
+            return entity is null ? null : MapToContract(entity);
+        }
+
+        private static PlayerResponseContract MapToContract(Speler entity)
+        {
+            return new PlayerResponseContract()
+            {
+                SpelerId = entity.SpelerId,
+                Voornaam = entity.Voornaam,
+                Naam = entity.Naam,
+                Aanwezigheids = (ICollection<AanwezigheidResponseContract>)entity.Aanwezigheids,
+                Dagklassements = (ICollection<DagKlassementResponseContract>)entity.Dagklassements,
+                Seizoensklassements = (ICollection<SeizoensKlassementResponseContract>)entity.Seizoensklassements
+
+            };
+        }
+    }
+}
