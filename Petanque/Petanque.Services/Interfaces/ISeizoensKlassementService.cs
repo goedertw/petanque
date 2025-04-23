983fd9cb5081f155ed1b298cf64164732015c028
Brent Opsomer <brent.opsomer@student.hogent.be>
2025-04-24 00:48:38 +0200

    seizoensklassement genereren en ophalen in backend

---
+++ Petanque/Petanque.Services/Interfaces/ISeizoensKlassementService.cs
@@ -0,0 +1,9 @@
+﻿namespace Petanque.Services.Interfaces;
+using Petanque.Contracts.Requests;
+using Petanque.Contracts.Responses;
+
+public interface ISeizoensKlassementService
+{
+    IEnumerable<SeizoensKlassementResponseContract> CreateSeizoensKlassementen(int seizoenId);
+    IEnumerable<SeizoensKlassementResponseContract> GetById(int seizoenId);
+}
